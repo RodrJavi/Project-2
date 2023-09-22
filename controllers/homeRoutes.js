b00a3bc7b96a7e9c202c1a7028b163b7b12bbfee
cgwol <cgwalterson@icloud.com>
2023-09-22 13:15:18 -0700

    Working posts on profile pages

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Post } = require("../models");
 const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
+router.get("/", withAuth, async (req, res) => {
   try {
     res.render("homepage", {
       logged_in: req.session.logged_in,
@@ -43,6 +43,7 @@ router.get('/profile', withAuth, async (req,res) =>{
 
     const userId = req.session.user_id;
 
+    //fetching the logged in user
     const dbUserData = await User.findByPk(userId, {
       attributes: ['username', 'displayName', 'email']
     })
@@ -50,7 +51,16 @@ router.get('/profile', withAuth, async (req,res) =>{
     const user = dbUserData.get({ plain: true });
     const postPartial = true;
 
-    res.render('profile', { user, postPartial, logged_in: req.session.logged_in });
+    // fetching the users posts
+    const dbPostData = await Post.findAll({
+      where: {user_id: userId},
+      include: User,
+      order: [['id', 'DESC']]
+    });
+
+    const userPosts = dbPostData.map((p) => p.get({ plain: true }));
+
+    res.render('profile', { user, userPosts, postPartial, logged_in: req.session.logged_in });
 
   }catch(error){
     res.status(500).json(error);
@@ -60,19 +70,30 @@ router.get('/profile', withAuth, async (req,res) =>{
 
 router.get('/profile/:username', withAuth, async (req, res) => {
   try{
+
+    // Fetching the user
     const reqUser = req.params.username;
     const dbUserData = await User.findOne({
       where: { username: reqUser },
-      attributes: ['username', 'displayName'] // Specify the attributes you want
+      attributes: ['id','username', 'displayName'] // Specify the attributes you want
     });
 
     const user = dbUserData.get({ plain: true });
 
+    const dbPostData = await Post.findAll({
+      where: {user_id: user.id},
+      include: User,
+      order: [['id', 'DESC']]
+    })
+
+    const userPosts = dbPostData.map((p) => p.get({ plain: true }));
+
     if(dbUserData){
-      res.render('profile', { user, logged_in: req.session.logged_in})
+      res.render('profile', { user, userPosts, logged_in: req.session.logged_in})
     }else{
       res.status(404).send('User not found');
     }
+
 
   }catch(error){
     res.status(500).json(error);
