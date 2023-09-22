e11ca5cdfc22c2d5f8d251c1ac10647e6b390245
cgwol <cgwalterson@icloud.com>
2023-09-21 21:07:52 -0700

    Fixed user Profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-<<<<<<< HEAD
 router.get("/", async (req, res) => {
-=======
-router.get('/', withAuth, async (req, res) => {
->>>>>>> b3f8db4cb82334860152a4b3497dd612f1bcbb4c
   try {
     res.render("homepage", {
       logged_in: req.session.logged_in,
@@ -16,30 +12,18 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
-=======
-router.get('/login', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
->>>>>>> b3f8db4cb82334860152a4b3497dd612f1bcbb4c
     return;
   }
 
   res.render("login");
 });
 
-<<<<<<< HEAD
 router.get("/signup", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/");
-=======
-router.get('/signup', (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect('/');
->>>>>>> b3f8db4cb82334860152a4b3497dd612f1bcbb4c
     return;
   }
 
@@ -48,17 +32,13 @@ router.get('/signup', (req, res) => {
 
 router.get('/profile', withAuth, async (req,res) =>{
   try{
-    const dbUserData = await User.findOne(req.params.username, {
-      include: [
-        {
-          model: User,
-          attributes: [
-            'username',
-            'displayName',
-            'email'
-          ]
-        }
-      ]
+
+    const userId = req.session.user_id;
+
+    console.log('\n\n '+userId+'\n\n');
+
+    const dbUserData = await User.findByPk(userId, {
+      attributes: ['username', 'displayName', 'email']
     })
 
     const user = dbUserData.get({ plain: true });
