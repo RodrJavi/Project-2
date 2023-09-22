d3e149a944fca2acfe4e1f3a9ab7c8b2dcfa5924
Javi <nb2javi@gmail.com>
2023-09-21 19:57:35 -0500

    Fixed logged in session in homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,33 @@
-const router = require('express').Router();
-const { User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    res.render('homepage', {
+    res.render("homepage", {
       logged_in: req.session.logged_in,
     });
-
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
     return;
   }
 
-  res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;
