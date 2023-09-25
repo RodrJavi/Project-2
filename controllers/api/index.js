8b7ea8346e11dfaab262cf426d2008ad313d8bb0
bloodymajima <mdeavers18@gmail.com>
2023-09-25 19:48:59 -0400

    Fixed weather API, encrypted key

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const weatherRoutes = require('./weatherRoutes');
 
 router.use('/users', userRoutes);
+router.use("/weather", weatherRoutes);
 
 module.exports = router;
+
